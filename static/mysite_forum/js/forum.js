a7e86a10beb61fe46139371600b536f498769da7
Abraham Lara Granados <abrahamlara19@gmail.com>
2018-08-27 23:39:29 -0400

    Changed the way the thread body is displayed when it has too much text. Made the thread page mobile compatible so that the body of text is scrollable.

--- static/mysite_forum/js/forum.js
+++ static/mysite_forum/js/forum.js
@@ -6,18 +6,26 @@ const fetchThreads = function() {
     $.ajax({
         url: 'threads/fetch_threads',
         contentType: 'application/json',
-        success: function(threads) {
-            displayThreads(JSON.parse(threads));
-        },
-        error: function(error) {
-            console.log(error);
-        }
+        success: displayThreads,
+        error: handleError
     });
 }
 
+const handleError = function(error) {
+    console.log(error);
+}
+
 const displayThreads = function(threads) {
+
+    threads  = JSON.parse(threads);
+
     for(i = threads.length-1; i >= 0; i--) {
         
+        /**
+         * Creates the card for each element to display the title and
+         * body for each thread while also creating a button to send the
+         * user to the thread page.
+         */
         card = $('<div>', {'class': 'card'});
         card_header = $('<div>', {'class': 'card-header', 'id': `${threads[i].pk}`});
         h5 = $('<h5>', {'class': 'mb-0'});
@@ -27,7 +35,19 @@ const displayThreads = function(threads) {
         card_body = $('<div>', {'class': 'card-body'});
 
         button.text(`${threads[i].fields.title}`);
-        card_body.text(`${threads[i].fields.body}`);
+
+        body = threads[i].fields.body;
+
+        /**
+         * If the body of the thread is too big to display, then this 'if'
+         * statement will reduce the amount of characters from the body and
+         * append '...' in front of the reduced body to imply that there is
+         * more text than what is displayed.
+         */
+        if(body.length > 1270) 
+            body = body.substring(0,1271)+'...';
+
+        card_body.text(`${body}`);
 
         collapse.append(card_body);
         h5.append(button,link);
@@ -36,8 +56,19 @@ const displayThreads = function(threads) {
 
         $('.accordion').append(card);
     }
+
+    if(threads.length == 1) $('.cards').addClass('btm-border');
 }
 
+/**
+ * @param {*} thread
+ * These constants define how the elements within a
+ * card should look and behave. Each card has it's
+ * own data associated with it that allows for the
+ * behaviour of displaying it's own body upon
+ * clicking its title and its own button that takes
+ * the user to its thread page.
+ */
 const buttonAttrs = function(thread) {
     return {
         'class': 'btn btn-link text-info title', 
