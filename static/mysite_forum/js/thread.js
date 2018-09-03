8c576478afb29049b45976a7dbfcb4f9d8a02dca
Abraham Lara Granados <abrahamlara19@gmail.com>
2018-09-03 00:46:10 -0400

    Implemented Reply buttons. These buttons currently log the id of the post it belongs to.

--- static/mysite_forum/js/thread.js
+++ static/mysite_forum/js/thread.js
@@ -3,21 +3,29 @@ $(document).ready(function() {
     displayPosts(posts);
 
     $('#post-btn').on('click', submitForm);
-    console.log(posts);
 });
 
 const displayPosts = function(posts) {
 
     for (i = posts.length-1; i >= 0; --i) {
         
-        post_cell = $('<div>', {'class': 'border border-info border-right-0 border-left-0 border-bottom-0 post-cell', 'id': posts[i].pk});
+        post_cell = $('<div>', {'class': 'border border-info border-right-0 border-left-0 border-bottom-0 post-cell', 'id': `post-cell-${posts[i].pk}`});
         post = $('<div>', {'class': 'post', 'text': posts[i].post});
-        author = $('<div>', {'class': 'author post-author', 'text': `- ${posts[i].author}`});
+        reply = $('<button>', {'class': 'btn btn-link text-info', 'value':`${posts[i].pk}`, 'text': 'Reply'});
+        author = $('<div>', {'class': 'author post-author'});
 
+        reply.on('click', displayReplyBox);
+
+        author.append(reply, ` - ${posts[i].author}`);
         post_cell.append(post,author);
 
         $('.posts-container').append(post_cell);
     }
+}
+
+const displayReplyBox = function(e) {
+    id = e.target.value;
+    console.log(id);
 }
 
 const submitForm = function() {
