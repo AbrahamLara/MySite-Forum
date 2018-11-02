b80ed7744b6a66a096bb02f3e7887808132104f8
Abraham Lara Granados <abrahamlara19@gmail.com>
2018-11-01 23:58:25 -0400

    Added toggle switch to display checkboxes for deletion of threads

--- static/mysite_forum/js/profile_auth.js
+++ static/mysite_forum/js/profile_auth.js
@@ -1,16 +1,25 @@
 $(document).ready(function() {
-   $('#optionDelete').on('click', displaySelections);
+   $('#default_check').on('click', displayChanges);
 });
 
-const displaySelections = function() {
-    $('.thread-link').addClass('hide');
-    $('.radio-container').removeClass('hide');
+const displayChanges = function() {
+    
+    if($(this).prop('checked')) {
+        test('.thread-link', '.radio-container');
+    } else {
+        test('.radio-container', '.thread-link');
+    }
+}
+
+const test = function(containerShow, containerHide) {
+    $(containerShow).addClass('hide');
+    $(containerHide).removeClass('hide');
 }
 
 const deleteSelection = function() {
     $.ajax({
         type: 'post',
-        url: '/delete_selection',
+        url: '/delete_selection/',
         success: function() {
         
         },
