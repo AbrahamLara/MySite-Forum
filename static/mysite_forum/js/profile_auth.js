9aa9a942764d76786c662ad83b78821f9fae2dd4
Abraham Lara Granados <abrahamlara19@gmail.com>
2018-11-04 21:06:32 -0500

    Thread blocks background colors change once selected

--- static/mysite_forum/js/profile_auth.js
+++ static/mysite_forum/js/profile_auth.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
-   $('#confirmDeletion').on('click', displayChanges);
-   
     for (var i = threads_context.threads.length-1; i >= 0; i--) {
         $('#threads-container').append(getThreadBlock(threads_context.threads[i]));
     }
 
+    $('#confirmDeletion').on('click', showModal);
+    $('#submit-btn').on('click', deleteSelection);
 });
 
-const displayChanges = function() {
+const showModal = function() {
     $('#confirm-modal').modal('show');
 }
 
@@ -15,6 +15,7 @@ const deleteSelection = function() {
     $.ajax({
         type: 'post',
         url: '/delete_selection/',
+        // data: {'threads': },
         success: function() {
         
         },
@@ -25,7 +26,7 @@ const deleteSelection = function() {
 }
 
 const getThreadBlock = function(thread_data) {
-    const thread_block = $('<div>', {'class': 'thread-block'});
+    const thread_block = $('<div>', {'class': 'thread-block', 'id': `thread-block-${thread_data.pk}`});
     const radio_container = $('<label>', {'class': 'radio-container'});
     const custom_checkbox = $('<div>', {'class': '', 'id': `custom-checkbox-${thread_data.pk}`});
     const thread_link = $('<a>', {'class': 'thread-link text-info', 'href': `/thread/${thread_data.pk}`});
@@ -34,9 +35,19 @@ const getThreadBlock = function(thread_data) {
 
     thread_link.text(thread_data.title);
 
+    checkbox.on('click', selectBlock);
+
     custom_checkbox.append(checkbox, checkmark);
     radio_container.append(thread_link, custom_checkbox);
     thread_block.append(radio_container);
 
     return thread_block;
+}
+
+const selectBlock = function(e) {
+    if ($(this).prop('checked')) {
+        $(`#thread-block-${e.target.value}`).addClass('selected');
+        console.log(e.target.value);
+    } else
+        $(`#thread-block-${e.target.value}`).removeClass('selected');
 }
